22290690a90d920141381a61535c431f53389eee
Paul Morrow <pmorow93@gmail.com>
2022-03-28 17:08:21 -0500

    Save Button

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Route, Link, Switch } from "react-router-dom";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
-import SavedList from './Movies/SavedList';
+import SavedList from "./Movies/SavedList";
 
-export default function App () {
+export default function App() {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const getMovies = () => {
       axios
-        .get('http://localhost:5001/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .get("http://localhost:5001/api/movies") // Study this endpoint with Postman
+        .then((response) => {
+          // console.log("response: ", response.data);
+          setMovieList(response.data);
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
         })
-        .catch(error => {
-          console.error('Server Error', error);
+        .catch((error) => {
+          console.error("Server Error", error);
         });
-    }
+    };
     getMovies();
   }, []);
 
-  const addToSavedList = id => {
+  const addToSavedList = (id) => {
+    setSaved(id);
+    console.log("ID: ", id);
+    const savedMovie = movieList.map((movie) => {
+      if (movie.id === id) {
+        return movie;
+      }
+    });
+    setSaved(savedMovie);
+    // return movie;
+
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
   return (
     <div>
-      <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <SavedList
+        list={
+          [
+            /* This is stretch */
+          ]
+        }
+      />
+      <Switch>
+        <Route path={"/movie/:id"}>
+          <Movie data={addToSavedList} />
+        </Route>
+        <Route path="/">
+          <MovieList movies={movieList} />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function SavedList(props) {
+  const history = useHistory();
+  const homeRoute = () => {
+    history.push("/");
+  };
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
+      {props.list.map((movie) => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <div onClick={homeRoute} className="home-button">
+        Home
+      </div>
     </div>
   );
 }
